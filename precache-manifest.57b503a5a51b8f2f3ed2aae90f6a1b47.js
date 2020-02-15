7ab383c3401b8889b263718e633d19a305c90301
Patryk Krajewski <patrykraj@interia.pl>
2020-02-15 23:00:48 +0100

    Updates

---
+++ precache-manifest.57b503a5a51b8f2f3ed2aae90f6a1b47.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "7db589e985b496231157",
+    "url": "/udemy-project-spa/static/css/main.779a95ff.chunk.css"
+  },
+  {
+    "revision": "7db589e985b496231157",
+    "url": "/udemy-project-spa/static/js/main.7f0c1dda.chunk.js"
+  },
+  {
+    "revision": "8c0c785baa203d287629",
+    "url": "/udemy-project-spa/static/js/runtime~main.918267b8.js"
+  },
+  {
+    "revision": "62047dece258f4869ce6",
+    "url": "/udemy-project-spa/static/js/2.f37d1b86.chunk.js"
+  },
+  {
+    "revision": "d3bd700078e7580c2910c9c8b3476644",
+    "url": "/udemy-project-spa/static/media/img1.d3bd7000.jpg"
+  },
+  {
+    "revision": "23fd3a5bd48283f42b64e7b58892187e",
+    "url": "/udemy-project-spa/static/media/img2.23fd3a5b.jpg"
+  },
+  {
+    "revision": "425a96222687018d8ec4ba90cbc41636",
+    "url": "/udemy-project-spa/static/media/img3.425a9622.jpg"
+  },
+  {
+    "revision": "20f4f09c0ddf20e1662fa6f787aec54e",
+    "url": "/udemy-project-spa/static/media/img4.20f4f09c.jpg"
+  },
+  {
+    "revision": "4b102e1863c45214f347dad660899c0b",
+    "url": "/udemy-project-spa/index.html"
+  }
+];
